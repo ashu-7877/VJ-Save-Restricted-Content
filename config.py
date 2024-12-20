57741300bb6771fc07bc7ed6f1c94c619793f81b
ashu-7877 <ashusharma62309@gmail.com>
2024-12-20 07:39:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "490ad5f5ed36ce93496c60ede5712081")
 ADMINS = int(os.environ.get("ADMINS", "684325515"))
 
 # Your Mongodb Database Url
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://ashusharma62309: hgcd9jxa5pYPisGl@cluster0.eyydm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://maafia194:NFdfkh4vKooi2qms@cluster0.sjj53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
